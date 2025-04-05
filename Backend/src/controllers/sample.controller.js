d87b8cdaa0419e29f955f4305506b87c14c46bce
unknown <shwetparkash91@gmail.com>
2025-04-05 20:12:18 +0530

    a

--- Backend/src/controllers/sample.controller.js
+++ Backend/src/controllers/sample.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asynchandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.model.js"
